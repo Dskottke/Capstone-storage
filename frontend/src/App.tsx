48139c6ac4b25b4ee4c51450a97469d7e269fe01
davidskottke <davidskottke@googlemail.com>
2022-11-18 12:53:17 +0100

    add test axios in frontend add proxy in package.json add axios dependency

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+
 import './App.css';
+import axios from "axios";
+import {Simulate} from "react-dom/test-utils";
+import error = Simulate.error;
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+const [data,setData] = useState()
+
+    const test = () => {
+        axios.get("/test")
+            .then(response => response.data)
+            .then(data => setData(data))
+    }
+
+    useEffect(test)
+    return (<>
+
+            {data}
+
+
+
+
+
+
+</>
   );
 }
 
