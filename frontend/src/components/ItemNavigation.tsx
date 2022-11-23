e5e56447e8ba52f0080ef5136eec359534fe2b33
davidskottke <davidskottke@googlemail.com>
2022-11-23 18:57:19 +0100

    add new error alert for response status 500

--- frontend/src/components/ItemNavigation.tsx
+++ frontend/src/components/ItemNavigation.tsx
@@ -1,6 +1,8 @@
 import React, {ChangeEvent, useState} from 'react';
 import axios from "axios";
 import "../css/ItemNavigation.css"
+import Alert from '@mui/material/Alert';
+
 
 type itemNavigationProbs = {
     fetchData: () => void
@@ -8,6 +10,8 @@ type itemNavigationProbs = {
 
 function ItemNavigation(props: itemNavigationProbs) {
     const [ean, setEan] = useState<string>()
+    const [failModal, setFailModal] = useState(false)
+    const [errorMessage, setErrorMessage] = useState("")
 
     const handleInputEan = (event: ChangeEvent<HTMLInputElement>) => {
         const validEan = event.target.value.replace(/\D/g, '')
@@ -18,7 +22,14 @@ function ItemNavigation(props: itemNavigationProbs) {
 
 
         axios.post("/api/items/" + ean)
-            .catch((error) => console.error("POST Error: " + error))
+            .catch(error => {
+                if (error.response.status === 500) {
+                    setFailModal(true);
+                    setErrorMessage("ean not found")
+
+                }
+            })
+
             .then(props.fetchData)
             .then(() => setEan(""))
     }
@@ -32,8 +43,14 @@ function ItemNavigation(props: itemNavigationProbs) {
                     <input value={ean}
                            onChange={handleInputEan} type="text" placeholder="ean" name="submit"/>
                     <button type="submit">add</button>
+
                 </form>
             </div>
+            {failModal &&
+                <Alert severity="error" onClose={() => {
+                    setFailModal(false)
+                }}>{errorMessage}</Alert>
+            }
         </div>
 
     );
