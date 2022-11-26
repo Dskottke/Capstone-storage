7115b0f91ab8f2a36edf38c19f66a949ab15bb89
davidskottke <davidskottke@googlemail.com>
2022-11-26 17:03:18 +0100

    add ItemValidationException and use it instead of IllegalArgumentException in ItemService.java

---
+++ backend/src/main/java/capstone/storage/backend/exceptions/ItemValidationException.java
@@ -0,0 +1,8 @@
+package capstone.storage.backend.exceptions;
+
+public class ItemValidationException extends RuntimeException {
+
+    public ItemValidationException(String message) {
+        super(message);
+    }
+}
