555b8af5522631c7452edbb9baef01f5b3798828
davidskottke <davidskottke@googlemail.com>
2022-11-28 17:16:49 +0100

    add TestDataService methods deleteAll,addItemData,addTestData add TestDataItemsNotFoundException

---
+++ backend/src/main/java/capstone/storage/backend/exceptions/TestDataItemsNotFoundException.java
@@ -0,0 +1,7 @@
+package capstone.storage.backend.exceptions;
+
+public class TestDataItemsNotFoundException extends RuntimeException {
+    public TestDataItemsNotFoundException(String message) {
+        super(message);
+    }
+}
