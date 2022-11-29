572dedc918d7a6ca6c35cdce38140eab6e06b6c2
davidskottke <davidskottke@googlemail.com>
2022-11-29 15:53:55 +0100

    add inline Styles of Card in MainPageItemCard.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import ItemPage from "./components/ItemPage";
 import './App.css';
 import MainPage from "./components/MainPage";
 import {Route, Routes} from "react-router-dom";
+import StoragePage from "./components/StoragePage";
 
 
 function App() {
@@ -12,8 +13,7 @@ function App() {
             <Routes>
                 <Route path={"/"} element={<MainPage/>}/>
                 <Route path={"/item-page"} element={<ItemPage/>}/>
-
-
+                <Route path={"/storage-page"} element={<StoragePage/>}/>
             </Routes>
 
         </>
--- frontend/src/components/MainPageItemCard.tsx
+++ frontend/src/components/MainPageItemCard.tsx
@@ -8,17 +8,17 @@ import item from '../images/item-logo.png'
 function MainPageItemCard() {
     return (
 
-        <Card sx={{maxWidth: 345, minHeight: 300, boxShadow: 5}}>
-                <div className={"item-card-header-blue"}>
-                    <img width={"40px"} src={item} alt={"item"}/>
-                </div>
-                <CardContent>
-                    <Typography variant="h5" component="div">
-                        Items
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        item overview
-                    </Typography>
+        <Card sx={{marginLeft: 10, minWidth: 345, minHeight: 400, boxShadow: 5}}>
+            <div className={"item-card-header-blue"}>
+                <img width={"40px"} src={item} alt={"item"}/>
+            </div>
+            <CardContent>
+                <Typography variant="h5" component="div">
+                    Items
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    item overview
+                </Typography>
                 </CardContent>
         </Card>
     );
