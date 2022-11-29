03b7d0b09128a574000f83ab10f46cc2fa1476a0
davidskottke <davidskottke@googlemail.com>
2022-11-29 14:52:41 +0100

    add StoragePage.tsx

---
+++ frontend/src/components/StoragePage.tsx
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+import axios from "axios";
+import {StorageBinModel} from "../model/StorageBinModel";
+import "../css/TablePage.css"
+import StorageTable from "./StorageTable";
+
+function StoragePage() {
+
+    const [data, setData] = useState<StorageBinModel[]>([]);
+
+    const fetchData = () => {
+        axios.get("/api/storage/")
+            .then(response => {
+                return response.data
+            })
+            .catch(error => {
+                return error
+            })
+            .then((data) => setData(data))
+    }
+
+    return (
+        <div className={"page-body"}>
+            <StorageTable fetchData={fetchData} data={data}/>
+        </div>
+    );
+}
+
+export default StoragePage;
