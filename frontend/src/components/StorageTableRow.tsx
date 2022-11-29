d4a482195ac6aa669a811286aaf5e081e6c63bcf
davidskottke <davidskottke@googlemail.com>
2022-11-29 15:13:02 +0100

    add StorageTableRow.tsx

---
+++ frontend/src/components/StorageTableRow.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {StorageBinModel} from "../model/StorageBinModel";
+
+type StorageTableRowProps = {
+    storageBin: StorageBinModel
+    fetchData: () => void
+}
+
+function StorageTableRow({storageBin, fetchData}: StorageTableRowProps) {
+
+    return (
+        <tr key={storageBin.id}>
+            <td>{storageBin.id}</td>
+            <td>{storageBin.itemNumber}</td>
+            <td>{storageBin.amount}</td>
+            <td>{storageBin.notified}</td>
+            <td>{storageBin.reserved}</td>
+        </tr>
+    );
+}
+
+export default StorageTableRow;
