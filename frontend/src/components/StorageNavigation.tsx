f3eff61c1fd9d3332f110c0ac3d61b54e6b22dcb
davidskottke <davidskottke@googlemail.com>
2022-11-29 16:48:21 +0100

    add styles

---
+++ frontend/src/components/StorageNavigation.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "../css/StorageNavigation.css"
+
+function StorageNavigation() {
+    return (
+        <div>
+            <div className="topnav">
+                <div id="navi-logo">Storage</div>
+                <a className="active-green" href="/">home</a>
+            </div>
+        </div>
+    );
+}
+
+export default StorageNavigation;
