66b9dce2b2db894af0b2b83043d2fd99df9fb9dd
davidskottke <davidskottke@googlemail.com>
2022-11-29 12:56:21 +0100

    add StorageBinController and GetRepquest

---
+++ backend/src/main/java/capstone/storage/backend/storagebin/StorageBinController.java
@@ -0,0 +1,21 @@
+package capstone.storage.backend.storagebin;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/storage-bin/")
+@RequiredArgsConstructor
+public class StorageBinController {
+
+    private final StorageBinService storageBinService;
+
+    @GetMapping()
+    public List<StorageBin> getAllStorageBins() {
+        return storageBinService.getAllStorageBins();
+    }
+}
