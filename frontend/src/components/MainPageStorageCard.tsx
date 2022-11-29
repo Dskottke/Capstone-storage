f9140d3701cbb82ea4e19722d3f58343199ee591
davidskottke <davidskottke@googlemail.com>
2022-11-29 16:06:14 +0100

    add styles for storage-logo

--- frontend/src/components/MainPageStorageCard.tsx
+++ frontend/src/components/MainPageStorageCard.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {Card, CardContent} from "@mui/material";
 import Typography from "@mui/material/Typography";
 import "../css/MainPageStorageCard.css"
+import storage from "../images/storage-logo.png";
 
 function MainPageStorageCard() {
     return (
         <Card sx={{marginLeft: 10, minWidth: 345, minHeight: 400, boxShadow: 5}}>
             <div className={"storage-card-header-red"}>
+                <img className={"storage-logo"} width={"30px"} src={storage} alt={"storage-logo"}/>
             </div>
             <CardContent>
                 <Typography variant="h5" component="div">
