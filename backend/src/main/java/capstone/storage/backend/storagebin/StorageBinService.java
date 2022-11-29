eeedf282bebfb8d68a3ccd29cd16d31ea3783c5a
davidskottke <davidskottke@googlemail.com>
2022-11-29 12:56:34 +0100

    add StorageBinService

---
+++ backend/src/main/java/capstone/storage/backend/storagebin/StorageBinService.java
@@ -0,0 +1,17 @@
+package capstone.storage.backend.storagebin;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StorageBinService {
+    private final StorageBinRepo repo;
+
+
+    public List<StorageBin> getAllStorageBins() {
+        return repo.findAll();
+    }
+}
