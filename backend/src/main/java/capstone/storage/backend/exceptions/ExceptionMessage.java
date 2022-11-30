5d400cfede1213390e0f149a1eac0091154e5ab0
davidskottke <davidskottke@googlemail.com>
2022-11-30 18:37:43 +0100

    add name_logo on MainPage

--- backend/src/main/java/capstone/storage/backend/exceptions/ExceptionMessage.java
+++ backend/src/main/java/capstone/storage/backend/exceptions/ExceptionMessage.java
@@ -1,7 +1,6 @@
 package capstone.storage.backend.exceptions;
 
 public enum ExceptionMessage {
-
     IS_NULL_OR_EMPTY_EXCEPTION_MESSAGE("all input-fields must be filled"),
     ITEM_VALIDATION_EXCEPTION_MESSAGE("capacity and the item-number must be greater than 0"),
     ITEM_ALREADY_EXIST_EXCEPTION_MESSAGE("item is already existing"),
@@ -10,7 +9,6 @@ public enum ExceptionMessage {
     ITEM_RESPONSE_EAN_NULL_EXCEPTION_MESSAGE("couldn't find item by ean"),
     EAN_API_RESPONSE_EXCEPTION_MESSAGE("couldn't find item by ean"),
     ITEM_NOT_FOUND_EXCEPTION_MESSAGE("ean to find doesn't match with response ean");
-
 
     private final String message;
 
