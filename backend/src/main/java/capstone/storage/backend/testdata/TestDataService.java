2a20b14d2a1a6236d93b7a6bd90a441a7fee7e5e
davidskottke <davidskottke@googlemail.com>
2022-11-30 10:14:39 +0100

    add delete all from storageBinRepo

--- backend/src/main/java/capstone/storage/backend/testdata/TestDataService.java
+++ backend/src/main/java/capstone/storage/backend/testdata/TestDataService.java
@@ -3,17 +3,18 @@ package capstone.storage.backend.testdata;
 import capstone.storage.backend.exceptions.TestDataItemsNotFoundException;
 import capstone.storage.backend.item.ItemRepo;
 import capstone.storage.backend.item.models.Item;
+import capstone.storage.backend.storagebin.StorageBinRepo;
 import capstone.storage.backend.utils.ServiceUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 public class TestDataService {
     private final ItemRepo itemRepo;
+    private final StorageBinRepo storageBinRepo;
     private final ServiceUtils serviceUtils;
 
     public void addTestData() {
@@ -25,11 +26,10 @@ public class TestDataService {
             throw new TestDataItemsNotFoundException("can't find data from json");
         }
     }
-
     public void deleteAll() {
         itemRepo.deleteAll();
+        storageBinRepo.deleteAll();
     }
-
     public List<Item> addListToItemDB(List<Item> itemList) {
 
         for (Item itemToAdd : itemList) {
