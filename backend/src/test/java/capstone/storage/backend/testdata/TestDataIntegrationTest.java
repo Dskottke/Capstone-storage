db2e463fbecb907b77fa4839ac3b714a3a742072
davidskottke <davidskottke@googlemail.com>
2022-12-01 12:57:33 +0100

    refactor code frontend

--- backend/src/test/java/capstone/storage/backend/testdata/TestDataIntegrationTest.java
+++ backend/src/test/java/capstone/storage/backend/testdata/TestDataIntegrationTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -18,6 +19,7 @@ class TestDataIntegrationTest {
 
     @Test
     @DisplayName("POST -> expect HTTP-status 204")
+    @DirtiesContext
     void PostAndCreateTestDataReturn204() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.post("/api/test-data"))
                 .andExpect(status().is(204));
