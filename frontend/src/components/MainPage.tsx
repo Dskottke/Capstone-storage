b5777bc4151ccee9505458a5722c0f08a63e6fc2
davidskottke <davidskottke@googlemail.com>
2022-12-01 17:30:23 +0100

    add Discharge And Store Cards

--- frontend/src/components/MainPage.tsx
+++ frontend/src/components/MainPage.tsx
@@ -4,6 +4,8 @@ import MainPageNavigation from "./MainPageNavigation"
 import "../css/MainPage.css";
 import {NavLink} from "react-router-dom";
 import MainPageStorageCard from "./MainPageStorageCard";
+import StoreOrderCard from "./StoreOrderCard";
+import DischargeOrderCard from "./DischargeOrderCard";
 
 
 function MainPage() {
@@ -15,6 +17,11 @@ function MainPage() {
             <div className={"main-page-container"}>
                 <NavLink to={"/item-page"} style={{textDecoration: 'none'}}><MainPageItemCard/></NavLink>
                 <NavLink to={"/storage-page"} style={{textDecoration: 'none'}}><MainPageStorageCard/></NavLink>
+                <NavLink to={"/storage-page"}
+                         style={{textDecoration: 'none', marginTop: 40}}><StoreOrderCard/></NavLink>
+                <NavLink to={"/storage-page"}
+                         style={{textDecoration: 'none', marginTop: 40}}><DischargeOrderCard/></NavLink>
+
             </div>
         </div>
 
