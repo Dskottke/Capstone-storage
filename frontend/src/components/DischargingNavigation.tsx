ba86e0259d17d2cf51ffb1b827c6ca80e8ded076
davidskottke <davidskottke@googlemail.com>
2022-12-01 18:04:06 +0100

    add DischargingTable add DischargingNavigation

---
+++ frontend/src/components/DischargingNavigation.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function DischargingNavigation() {
+    return (
+        <div>
+            <div>
+                <div className="topnav">
+                    <div id="navi-logo">discharging-orders</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default DischargingNavigation;
---
+++ frontend/src/components/DischargingTable.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function DischargingTable() {
+    return (
+        <div className="container-table">
+            <table className="Table">
+                <thead>
+                <tr>
+                    <th>Nr</th>
+                    <th>storage-bin</th>
+                    <th>item</th>
+                    <th>amount</th>
+                    <th>action</th>
+                </tr>
+                </thead>
+            </table>
+        </div>
+    );
+}
+
+export default DischargingTable;
