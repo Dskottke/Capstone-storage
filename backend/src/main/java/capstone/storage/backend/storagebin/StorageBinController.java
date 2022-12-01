974ea1875f7de7fef565540c896f14fbe12743ac
davidskottke <davidskottke@googlemail.com>
2022-12-01 09:42:48 +0100

    change path-naming api/storage to api/storagebins

--- backend/src/main/java/capstone/storage/backend/storagebin/StorageBinController.java
+++ backend/src/main/java/capstone/storage/backend/storagebin/StorageBinController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/storage/")
+@RequestMapping("api/storagebins/")
 @RequiredArgsConstructor
 public class StorageBinController {
     private final StorageBinService storageBinService;
