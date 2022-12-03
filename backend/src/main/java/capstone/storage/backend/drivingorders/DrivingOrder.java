8ba33c1ed333833a1146d6b5e9315070309347a3
davidskottke <davidskottke@googlemail.com>
2022-12-03 18:55:30 +0100

    add GetMapping with RequestParam type

--- backend/src/main/java/capstone/storage/backend/drivingorders/DrivingOrder.java
+++ backend/src/main/java/capstone/storage/backend/drivingorders/DrivingOrder.java
@@ -1,14 +1,10 @@
 package capstone.storage.backend.drivingorders;
 
-
-import capstone.storage.backend.item.models.Item;
-import capstone.storage.backend.storagebin.StorageBin;
-
 public record DrivingOrder(
         String id,
-        StorageBin storageBin,
-        Item item,
-        DrivingOrderType drivingOrderType,
+        String storageBinNumber,
+        String itemNumber,
+        Type type,
         String amount
 ) {
 }
--- backend/src/main/java/capstone/storage/backend/drivingorders/DrivingOrderController.java
+++ backend/src/main/java/capstone/storage/backend/drivingorders/DrivingOrderController.java
@@ -3,18 +3,19 @@ package capstone.storage.backend.drivingorders;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("api/driving-order/")
+@RequestMapping("api/driving-order")
 @RequiredArgsConstructor
 public class DrivingOrderController {
     private final DrivingOrderService drivingOrderService;
 
-    @GetMapping("/input")
-    public List<DrivingOrder> getAllInputDrivingOrders() {
-        return drivingOrderService.getAllInputDrivingOrders();
+    @GetMapping
+    public List<DrivingOrder> getAllDrivingOrdersByType(@RequestParam Type type) {
+        return drivingOrderService.getAllDrivingOrdersByType(type);
     }
 }
