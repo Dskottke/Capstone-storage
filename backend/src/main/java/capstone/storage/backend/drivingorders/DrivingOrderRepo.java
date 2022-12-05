4aa44761e8ed3e4128ab1d4b48523a3c67c101dd
davidskottke <davidskottke@googlemail.com>
2022-12-05 13:40:55 +0100

    refactor Table in StoringTableRow.tsx refactor logic in DrivingOrderService

--- backend/src/main/java/capstone/storage/backend/drivingorders/DrivingOrderRepo.java
+++ backend/src/main/java/capstone/storage/backend/drivingorders/DrivingOrderRepo.java
@@ -4,9 +4,12 @@ import capstone.storage.backend.drivingorders.models.DrivingOrder;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DrivingOrderRepo extends MongoRepository<DrivingOrder, String> {
    List<DrivingOrder> findByType(Type type);
 
    List<DrivingOrder> findByTypeAndStorageBinNumber(Type type, String storageBinNumber);
+
+   Optional<DrivingOrder> findFirstByStorageBinNumber(String storageBinNumber);
 }
