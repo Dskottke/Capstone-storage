729c67ff84d98c2ff677bc69afc5e230efd34e13
davidskottke <davidskottke@googlemail.com>
2022-12-07 16:05:47 +0100

    add method findAllByItemNumber in StorageBinRepo.java add method getItemAmountFromStorageByItemNumber in StorageBinService.java refactor getAll method in ItemService.java

--- backend/src/main/java/capstone/storage/backend/item/ItemService.java
+++ backend/src/main/java/capstone/storage/backend/item/ItemService.java
@@ -5,10 +5,12 @@ import capstone.storage.backend.exceptions.ItemValidationException;
 import capstone.storage.backend.item.models.AddItemDto;
 import capstone.storage.backend.item.models.Item;
 import capstone.storage.backend.item.models.Product;
+import capstone.storage.backend.storagebin.StorageBinService;
 import capstone.storage.backend.utils.ServiceUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,10 +18,30 @@ import java.util.List;
 public class ItemService {
     private final ItemRepo repository;
     private final ItemEanApiService eanService;
+    private final StorageBinService storageBinService;
     private final ServiceUtils utils;
 
     public List<Item> findAll() {
-        return repository.findAll();
+        List<Item> allItems = repository.findAll();
+        List<Item> listToReturn = new ArrayList<>();
+
+        for (Item item : allItems) {
+            String amount = storageBinService.getItemAmountFromStorageBinsByItemNumber(item);
+
+            Item itemToAdd = new Item(
+                    item.id(),
+                    item.name(),
+                    item.categoryName(),
+                    item.issuingCountry(),
+                    item.ean(),
+                    item.storableValue(),
+                    item.itemNumber(),
+                    amount);
+            listToReturn.add(itemToAdd);
+        }
+
+        return listToReturn;
+
     }
 
     public Item addItem(AddItemDto addItemDto, String eanToFind) {
@@ -30,6 +52,7 @@ public class ItemService {
         Product product = eanService.getItemResponseFromApi(eanToFind);
 
 
+        String defaultStoringBinAmount = "0";
         Item itemToAdd = new Item(
                 utils.generateUUID(),
                 product.name(),
@@ -37,7 +60,9 @@ public class ItemService {
                 product.issuingCountry(),
                 product.ean(),
                 addItemDto.storableValue(),
-                addItemDto.itemNumber());
+                addItemDto.itemNumber(),
+                defaultStoringBinAmount);
+
         return repository.insert(itemToAdd);
     }
 
