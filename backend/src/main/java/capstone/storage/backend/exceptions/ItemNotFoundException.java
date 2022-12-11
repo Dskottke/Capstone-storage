e3499b370f80e2e4100e5ac67490a32c5dbae492
davidskottke <davidskottke@googlemail.com>
2022-12-12 00:16:40 +0100

    add new ItemNotFound Exception

---
+++ backend/src/main/java/capstone/storage/backend/exceptions/ItemNotFoundException.java
@@ -0,0 +1,7 @@
+package capstone.storage.backend.exceptions;
+
+public class ItemNotFoundException extends RuntimeException {
+    public ItemNotFoundException() {
+        super("Can't find item!");
+    }
+}
