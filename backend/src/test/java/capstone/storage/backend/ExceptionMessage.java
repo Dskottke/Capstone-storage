02735471f765508dbe176118884cf039baa6680e
davidskottke <davidskottke@googlemail.com>
2022-12-12 00:16:25 +0100

    add new ItemNotFound Exception

--- backend/src/test/java/capstone/storage/backend/ExceptionMessage.java
+++ backend/src/test/java/capstone/storage/backend/ExceptionMessage.java
@@ -7,7 +7,6 @@ public enum ExceptionMessage {
     IS_NULL_OR_EMPTY_EXCEPTION_MESSAGE("all input-fields must be filled"),
     ITEM_ALREADY_EXIST_EXCEPTION_MESSAGE("item is already existing"),
     ITEM_FORBIDDEN_REQUEST_EXCEPTION_MESSAGE("forbidden request"),
-    ITEM_NOT_FOUND_EXCEPTION_MESSAGE("ean to find doesn't match with response ean"),
     ITEM_RESPONSE_EAN_NULL_EXCEPTION_MESSAGE("couldn't find item by ean"),
     ITEM_TO_DELETE_NOT_FOUND_EXCEPTION_MESSAGE("item is already deleted"),
     ITEM_VALIDATION_EXCEPTION_MESSAGE("capacity and the item-number must be greater than 0"),
@@ -16,10 +15,11 @@ public enum ExceptionMessage {
     STORAGE_BIN_FALSE_ITEM_EXCEPTION_MESSAGE("storage-bin is filled with not matching item"),
     TEST_DATA_ITEMS_NOT_FOUND_MESSAGE("can't find data from json"),
     NOT_ENOUGH_ITEMS_REMAINING_EXCEPTION_MESSAGE("not enough items remaining"),
-    STORED_ITEMS_EXCEPTION("can't delete because there are open orders or items stored"),
+    STORED_ITEMS_EXCEPTION_MESSAGE("can't delete because there are open orders or items stored"),
     ITEM_IS_NOT_EXISTING_MESSAGE("item is not existing"),
     ILLEGAL_TYPE_EXCEPTION_MESSAGE("The type is not allowed!"),
-    STORAGE_BIN_NOT_FOUND_EXCEPTION("Can't find storage-bin.");
+    STORAGE_BIN_NOT_FOUND_EXCEPTION_MESSAGE("Can't find storage-bin."),
+    ITEM_NOT_FOUND_EXCEPTION_MESSAGE("Can't find item!");
 
     private final String message;
 
