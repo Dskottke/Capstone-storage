3aab2dcb0202f334d18ade7422c58a493fcefdc0
davidskottke <davidskottke@googlemail.com>
2022-12-12 00:00:31 +0100

    fix bug in ItemTableRow.tsx

--- backend/src/main/java/capstone/storage/backend/exceptions/IllegalTypeException.java
+++ backend/src/main/java/capstone/storage/backend/exceptions/IllegalTypeException.java
@@ -1,6 +1,5 @@
 package capstone.storage.backend.exceptions;
 
-
 public class IllegalTypeException extends RuntimeException {
     public IllegalTypeException() {
         super("The type is not allowed!");
