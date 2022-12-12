1f1b60e5df2b24c59f42dde0c66cad984f87e7c8
davidskottke <davidskottke@googlemail.com>
2022-12-12 15:41:13 +0100

    refactor error messages

--- backend/src/main/java/capstone/storage/backend/exceptions/NotEnoughItemsRemainingException.java
+++ backend/src/main/java/capstone/storage/backend/exceptions/NotEnoughItemsRemainingException.java
@@ -1,7 +1,7 @@
 package capstone.storage.backend.exceptions;
 
 public class NotEnoughItemsRemainingException extends RuntimeException {
-    public NotEnoughItemsRemainingException() {
-        super("Not enough items remaining.");
+    public NotEnoughItemsRemainingException(String locationId) {
+        super("Not enough items remaining on storageBin with ID: " + locationId);
     }
 }
